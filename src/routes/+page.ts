5c843f98412a622fea998dbddc959a01a0e664af
Karzan Botani <karzan@botani.nu>
2022-11-17 20:07:41 +0100

    pushed stuff from cloudflare docs

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,8 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+export async function post({ request, platform }) {
+	const counter = platform.env.COUNTER.idFromName('A');
+	console.log('post -> counter', counter);
+}
